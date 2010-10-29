90b5d3fa2020a5c74a427c21e2d4fddc7b43aadc
Alexandre Testu <atestu@gmail.com>
2010-10-30 00:24:11 +0200

    I hate myself sometimes. to_s. to_i. Dummy.

--- hello.rb
+++ hello.rb
@@ -5,15 +5,15 @@ require 'rest-open-uri'
 
 get '/:city' do
   doc = open("http://www.google.com/ig/api?weather=#{params[:city].gsub(" ", "%20")}") { |f| Hpricot(f) }
-  @temperature = doc.at("temp_c")['data']
-  @farenheit = doc.at("temp_f")['data']
+  @temperature = doc.at("temp_c")['data'].to_i
+  @farenheit = doc.at("temp_f")['data'].to_i
   
-  @message1 = "It's " + @temperature + "°C/" + @farenheit + "°F outside, so… "
+  @message1 = "It's " + @temperature.to_s + "°C/" + @farenheit.to_s + "°F outside, so… "
   @message2 = ""
-  if @temperature > "15" then
+  if @temperature > 15 then
     @yesorno = "No Coat"
   else
-    if @temperature > "10"
+    if @temperature > 10
       @yesorno = "Meh"
       @message2 = "If you put one on, you'll be too hot and if you don't you'll be cold.<br /> Life's a bitch sometimes."
     else
