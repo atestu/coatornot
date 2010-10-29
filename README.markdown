86c8df105eae329d43dda617df859c5c83cc0828
Alexandre Testu <atestu@gmail.com>
2010-10-29 22:04:16 +0200

    works ok. no design. two weird errors in sinatra console (osef?)

---
+++ README.markdown
@@ -0,0 +1,8 @@
+CoatOrNot?
+============
+
+CoatOrNot is a small webapp written in Ruby using [Sinatra](http://www.sinatrarb.com/).
+
+"Quick and dirty" is an appropriate expression to describe the development process.
+
+I just wanted to create a simple application and deploy it really fast. It's hosted by [Heroku](http://heroku.com/), a service I strongly recommend.
---
+++ hello.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'sinatra'
+require 'hpricot'
+require 'open-uri'
+
+mime_type '.otf',  "application/x-font-otf"
+
+get '/:city' do
+  doc = open("http://www.google.com/ig/api?weather=#{params[:city]}") { |f| Hpricot(f) }
+  @temperature = doc.at("temp_c")['data']
+  
+  if @temperature > "15" then
+    @yesorno = "No"
+  else
+    if @temperature > "10"
+      @yesorno = "Meh"
+    else
+      @yesorno = "Yes"
+    end
+  end
+  @message = "It's " + @temperature + "°C outside."
+  erb :hello
+end
