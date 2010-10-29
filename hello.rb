8d8cf873a655b0cbbcf008567aa567a9b10a0614
Alexandre Testu <atestu@gmail.com>
2010-10-29 23:21:54 +0200

    1.0

--- hello.rb
+++ hello.rb
@@ -3,21 +3,33 @@ require 'sinatra'
 require 'hpricot'
 require 'rest-open-uri'
 
-mime_type '.otf',  "application/x-font-otf"
-
 get '/:city' do
-  doc = open("http://www.google.com/ig/api?weather=#{params[:city]}") { |f| Hpricot(f) }
+  doc = open("http://www.google.com/ig/api?weather=#{params[:city].gsub(" ", "%20")}") { |f| Hpricot(f) }
   @temperature = doc.at("temp_c")['data']
+  @farenheit = doc.at("temp_f")['data']
   
+  @message1 = "It's " + @temperature + "°C/" + @farenheit + "°F outside, so… "
+  @message2 = ""
   if @temperature > "15" then
-    @yesorno = "No"
+    @yesorno = "No coat"
   else
     if @temperature > "10"
       @yesorno = "Meh"
+      @message2 = "If you put one on, you'll be too hot and if you don't you'll be cold.<br /> Life's a bitch sometimes."
     else
-      @yesorno = "Yes"
+      @yesorno = "Coat"
     end
   end
-  @message = "It's " + @temperature + "°C outside."
+  @city = params[:city]
+
   erb :hello
+end
+
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  redirect "/#{params[:post][:city]}"
 end
