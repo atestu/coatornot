684e22b185e8d7dfca455828ca020cd434898a08
Alexandre Testu <atestu@gmail.com>
2010-10-30 19:05:52 +0200

    Kevin can be a bit of an asshole sometimes.

--- hello.rb
+++ hello.rb
@@ -5,24 +5,28 @@ require 'rest-open-uri'
 
 get '/:city' do
   doc = open("http://www.google.com/ig/api?weather=#{params[:city].gsub(" ", "%20")}") { |f| Hpricot(f) }
-  @temperature = doc.at("temp_c")['data'].to_i
-  @farenheit = doc.at("temp_f")['data'].to_i
+  unless (doc/"problem_cause") then
+    @temperature = doc.at("temp_c")['data'].to_i
+    @farenheit = doc.at("temp_f")['data'].to_i
   
-  @message1 = "It's " + @temperature.to_s + "°C/" + @farenheit.to_s + "°F outside, so… "
-  @message2 = ""
-  if @temperature > 15 then
-    @yesorno = "No Coat"
-  else
-    if @temperature > 10
-      @yesorno = "Meh"
-      @message2 = "If you put one on, you'll be too hot and if you don't you'll be cold.<br /> Life's a bitch sometimes."
+    @message1 = "It's " + @temperature.to_s + "°C/" + @farenheit.to_s + "°F outside, so… "
+    @message2 = ""
+    if @temperature > 15 then
+      @yesorno = "No Coat"
     else
-      @yesorno = "Coat"
+      if @temperature > 10
+        @yesorno = "Meh"
+        @message2 = "If you put one on, you'll be too hot and if you don't you'll be cold.<br /> Life's a bitch sometimes."
+      else
+        @yesorno = "Coat"
+      end
     end
-  end
-  @city = params[:city]
+    @city = params[:city]
 
-  erb :hello
+    erb :hello
+  else
+    erb :dumbass
+  end
 end
 
 
